39d0f9b775233eba6c9368be1a156761750bbd12
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-11 07:14:40 +0530

    Project setup done

---
+++ HRS/main/urls.py
@@ -0,0 +1,11 @@
+from unicodedata import name
+from .import views
+from unicodedata import name
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+
+
+urlpatterns = [
+    path('',views.index,name='index')
+]
--- HRS/main/views.py
+++ HRS/main/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse("HELLO")
