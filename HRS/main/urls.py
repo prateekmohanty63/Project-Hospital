ad0fc0d7c4b14d14f80a0c88f2c6919f0494fa07
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-06-14 21:47:17 +0530

    custom user form removed

--- HRS/main/urls.py
+++ HRS/main/urls.py
@@ -8,6 +8,7 @@ from .views import *
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('signin/',views.signin,name='signin'),
-    path('signup/',views.signup,name='signup')
+    path('signin/',views.signIn,name='signin'),
+    path('signup/',views.signUp,name='signup'),
+    path('userReg/',views.userRegistration,name='user-registration')
 ]
--- HRS/main/views.py
+++ HRS/main/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 
 # Create your views here.
@@ -8,8 +8,11 @@ from django.http import HttpResponse
 def index(request):
     return render(request,'index.html')
 
-def signin(request):
+def signIn(request):
     return render(request,'signin.html')
 
-def signup(request):
+def signUp(request):
     return render(request,'signup.html')
+
+def userRegistration(request):
+    return render(request,"user_registration.html")
